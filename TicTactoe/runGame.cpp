d2457a81981c6c9ca83bed47e32db9af2b167d2b
v1ll3m <Ville-Matti_S@hotmail.com>
2022-07-15 15:23:38 +0300

    Add project files.

---
+++ TicTactoe/runGame.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int const COLUMN = 3;
+int const ROW = 3;
+char gameBoard[COLUMN][ROW];
+
+
+
+void drawGameBoard(char arrayBoard[COLUMN][ROW]);
+void runGame();
+bool checkWin();
+
+int main()
+{
+	rungame();
+
+	return 0;
+}
+
+
